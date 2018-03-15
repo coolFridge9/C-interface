a5b25ac8ddaeee34fac9783709785818c98288ac
jordan elley <jordan.elley@myob.com>
2018-03-15 13:18:42 +1300

    created an interface

---
+++ InterfaceLearning/InterfaceLearning/ILearn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace InterfaceLearning
+{
+    public interface ITransactions
+    {
+        //interface members
+        void showTransaction();
+        double getAmount();
+    }
+
+    public class Transaction : ITransactions
+    {
+        private string tCode;
+        private string date;
+        private double amount;
+
+        public Transaction()
+        {
+            tCode = " ";
+            date = " ";
+            amount = 0.0;
+        }
+
+        public Transaction(string c, string d, double a)
+        {
+            tCode = c;
+            date = d;
+            amount = a;
+        }
+        
+        public void showTransaction()
+        {
+            Console.WriteLine("Transaction: {0}", tCode);
+        }
+
+        public double getAmount()
+        {
+            return amount;
+        }
+    }
+}
---
+++ InterfaceLearning/InterfaceLearning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterfaceLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Transaction("001","8/10/2012",789000.00);
+            var t2 = new Transaction("002","9/10/2012", 45100.00);
+            t1.showTransaction();
+            t2.showTransaction();
+            Console.ReadKey();
+        }
+    }
+}
