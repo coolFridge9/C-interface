fe05753cafa631cbe22805f9ee223044428a3738
jordan elley <jordan.elley@myob.com>
2018-03-27 10:07:39 +1300

    before the silly client changed their mind

--- GameOfLife/TestProject1/UnitTest1.cs
+++ GameOfLife/TestProject1/UnitTest1.cs
@@ -1,14 +1,54 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace TestProject1
 {
-    public class UnitTest1
+    public class GridShould
     {
         [Fact]
-        public void Test1()
+        public void ComputeNextStage()
         {
+            var points = new List<Point>
+            {
+                new Point(0,0),
+                new Point(1,2)
+                
+            };
+            var grid = new Grid(points);
+            var result = grid.NextState();
+            var expected = new List<Point>();
             
+            
+
+
+        }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Grid
+    {
+        public int GridSize = 4;
+
+        public Grid(List<Point> points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object NextState()
+        {
+            throw new NotImplementedException();
         }
     }
 }
